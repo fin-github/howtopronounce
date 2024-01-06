65191efe4dffc0595abc1c92ea86ca302e743b62
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2024-01-06 12:51:27 -0500

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,23 @@
+import pyttsx3
+from sys import argv
+from os import system as cmd
+from simplefuncs import *
+from imgmaker import makeimg
+from makevid import combine
+
+tts = pyttsx3.init()
+args = argv
+args.pop(0)
+
+try:
+    word = args[0]
+except IndexError:
+    word = input("Please enter a word: ")
+print(f"Now starting with {word}")
+say(word)
+
+
+# main thing
+savetofile(word, "tmp/word.mp3")
+makeimg(word, "tmp/img.png")
+combine("tmp/img.png", "tmp/word.mp3", word=word)
