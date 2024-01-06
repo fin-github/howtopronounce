8b9d1e3e63117cdec86bf8e7963c279de573d09d
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2024-01-06 14:56:54 -0500

    Update howtopronounce.py

--- howtopronounce.py
+++ howtopronounce.py
@@ -3,11 +3,13 @@ from imgmaker import makeimg
 from makevid import combine
 
 def create(word:str, ttspath:str = None, imgpath:str = None, check:bool = False):
-    if ttspath is None: ttspath = f"tmp/{word}tts.mp3"
-    if ttspath is None: imgpath = f"tmp/{word}img.mp3"
-    savetofile(word, ttspath)
-    makeimg(word, imgpath)
-    combine(imgpath, ttspath, word=word)
+    if ttspath is None: fixedttspath = f"tmp/{word}tts.mp3"
+    else: fixedttspath = ttspath
+    if ttspath is None: fixedimgpath = f"tmp/{word}img.png"
+    else: fixedimgpath = imgpath
+    savetofile(word, fixedttspath)
+    makeimg(word, fixedimgpath)
+    combine(fixedimgpath, fixedttspath, word=word)
     if not check: return
     with open(ttspath, "r") as file:
         if file.read() == "": return 1
